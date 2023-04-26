4e109ec75177565c164735283df6b6a6fc13d44e
Shaumik-Ashraf <shaumikashraf@mitre.org>
2023-04-26 16:08:15 -0400

    hard-code order in mappings.js (but not content)

--- modules/mappings.js
+++ modules/mappings.js
@@ -2,123 +2,150 @@ const role_module_map = [
   {
     "role": "clinician-scientist",
     "modules": [
-        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
-        {"slug": "terminology",                         "text": "Terminology"},
-        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
-        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
-        {"slug": "redcap-example",                      "text": "REDCap Examples"},
-        {"slug": "key-fhir-resources",                  "text": "Key FHIR Resources"},
-        {"slug": "fhir-history",                        "text": "History of FHIR"},
-        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
-        {"slug": "cdms",                                "text": "Common Data Models"},
-        {"slug": "fhir-research",                       "text": "FHIR for Research"},
-        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
         {"slug": "fhir-from-10000-ft",                  "text": "FHIR from 10,000ft"},
-        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"},
-        {"slug": "redcap-intro",                        "text": "Introduction to REDCap"},
-        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
-        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
-        {"slug": "data-modeling-reading-igs",           "text": "Data Modeling: Reading IGs"},
+        {"slug": "fhir-history",                        "text": "History of FHIR"},
+        {"slug": "terminology",                         "text": "Terminology"},
+        {"slug": "key-fhir-resources",                  "text": "Key FHIR Resources"},
+        {"slug": "fhir-research",                       "text": "FHIR for Research"},
         {"slug": "example-study",                       "text": "Examples of Real World Studies"},
+
+        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
+        {"slug": "data-modeling-reading-igs",           "text": "Data Modeling: Reading IGs"},
+        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
         {"slug": "data-modeling-writing-igs",           "text": "Data Modeling: Writing IGs"},
-        {"slug": "tabular-real-world",                  "text": "Tabular Data: Real World Cases"}
+
+        {"slug": "cdms",                                "text": "Common Data Models"},
+
+        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
+        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
+        {"slug": "tabular-real-world",                  "text": "Tabular Data: Real World Cases"},
+
+        {"slug": "redcap-intro",                        "text": "Introduction to REDCap"},
+        {"slug": "redcap-example",                      "text": "REDCap Examples"},
+        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
+
+        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
+        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
+        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"}
     ]
   },
   {
     "role": "informaticist",
     "modules": [
-        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
-        {"slug": "terminology",                         "text": "Terminology"},
-        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
-        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
-        {"slug": "redcap-example",                      "text": "REDCap Examples"},
-        {"slug": "key-fhir-resources",                  "text": "Key FHIR Resources"},
+        {"slug": "fhir-from-10000-ft",                  "text": "FHIR from 10,000 Feet"},
         {"slug": "fhir-history",                        "text": "History of FHIR"},
-        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
-        {"slug": "cdms",                                "text": "Common Data Models"},
+        {"slug": "terminology",                         "text": "Terminology"},
+        {"slug": "key-fhir-resources",                  "text": "Key FHIR Resources"},
         {"slug": "fhir-community",                      "text": "FHIR Community"},
         {"slug": "fhir-research",                       "text": "FHIR for Research"},
-        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
-        {"slug": "fhir-from-10000-ft",                  "text": "FHIR from 10,000 Feet"},
-        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"},
-        {"slug": "redcap-intro",                        "text": "Introducion to REDCap"},
-        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
-        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
-        {"slug": "data-modeling-reading-igs",           "text": "Data Modeling: Reading IGs"},
         {"slug": "example-study",                       "text": "Examples of Real World Studies"},
+
+        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
+        {"slug": "data-modeling-reading-igs",           "text": "Data Modeling: Reading IGs"},
+        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
         {"slug": "data-modeling-writing-igs",           "text": "Data Modeling: Writing IGs"},
-        {"slug": "tabular-real-world",                  "text": "Tabular Data: Real World Cases"}
+
+        {"slug": "cdms",                                "text": "Common Data Models"},
+
+        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
+        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
+        {"slug": "tabular-real-world",                  "text": "Tabular Data: Real World Cases"},
+
+        {"slug": "redcap-intro",                        "text": "Introducion to REDCap"},
+        {"slug": "redcap-example",                      "text": "REDCap Examples"},
+        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
+
+        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
+        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
+        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"}
+
     ]
   },
   {
     "role": "investigator",
     "modules": [
-        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
-        {"slug": "terminology",                         "text": "Terminology"},
-        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
-        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
-        {"slug": "redcap-example",                      "text": "REDCap Examples"},
-        {"slug": "fhir-history",                        "text": "History of FHIR"},
-        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
-        {"slug": "cdms",                                "text": "Common Data Models"},
-        {"slug": "fhir-research",                       "text": "FHIR for Research"},
-        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
         {"slug": "fhir-from-10000-ft",                  "text": "FHIR from 10,000 Feet"},
-        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"},
-        {"slug": "redcap-intro",                        "text": "Introducion to REDCap"},
-        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
-        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
-        {"slug": "data-modeling-reading-igs",           "text": "Data Modeling: Reading IGs"},
+        {"slug": "fhir-history",                        "text": "History of FHIR"},
+        {"slug": "terminology",                         "text": "Terminology"},
+        {"slug": "fhir-research",                       "text": "FHIR for Research"},
         {"slug": "example-study",                       "text": "Examples of Real World Studies"},
+
+
+        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
+        {"slug": "data-modeling-reading-igs",           "text": "Data Modeling: Reading IGs"},
+        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
         {"slug": "data-modeling-writing-igs",           "text": "Data Modeling: Writing IGs"},
-        {"slug": "tabular-real-world",                  "text": "Tabular Data: Real World Cases"}
+
+        {"slug": "cdms",                                "text": "Common Data Models"},
+
+        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
+        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
+        {"slug": "tabular-real-world",                  "text": "Tabular Data: Real World Cases"},
+
+        {"slug": "redcap-intro",                        "text": "Introducion to REDCap"},
+        {"slug": "redcap-example",                      "text": "REDCap Examples"},
+        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
+
+        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
+        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
+        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"}
     ]
   },
   {
     "role": "research-leader",
     "modules": [
-        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
-        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
-        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
-        {"slug": "redcap-example",                      "text": "REDCap Examples"},
-        {"slug": "fhir-history",                        "text": "History of FHIR"},
-        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
-        {"slug": "cdms",                                "text": "Common Data Models"},
-        {"slug": "fhir-research",                       "text": "FHIR for Research"},
-        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
         {"slug": "fhir-from-10000-ft",                  "text": "FHIR from 10,000 Feet"},
-        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"},
-        {"slug": "redcap-intro",                        "text": "Introducion to REDCap"},
-        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
-        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
+        {"slug": "fhir-history",                        "text": "History of FHIR"},
+        {"slug": "fhir-research",                       "text": "FHIR for Research"},
         {"slug": "example-study",                       "text": "Examples of Real World Studies"},
+
+        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
+        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
         {"slug": "data-modeling-writing-igs",           "text": "Data Modeling: Writing IGs"},
+
+        {"slug": "cdms",                                "text": "Common Data Models"},
+
+        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
+        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
+
+        {"slug": "redcap-intro",                        "text": "Introducion to REDCap"},
+        {"slug": "redcap-example",                      "text": "REDCap Examples"},
+        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
+
+        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
+        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
+        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"}
     ]
   },
   {
     "role": "software-engineer",
     "modules": [
-        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
-        {"slug": "terminology",                         "text": "Terminology"},
-        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
-        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
-        {"slug": "redcap-example",                      "text": "REDCap Examples"},
-        {"slug": "key-fhir-resources",                  "text": "Key FHIR Resources"},
+        {"slug": "fhir-from-10000-ft",                  "text": "FHIR from 10,000ft"},
         {"slug": "fhir-history",                        "text": "History of FHIR"},
-        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
-        {"slug": "cdms",                                "text": "Common Data Models"},
+        {"slug": "terminology",                         "text": "Terminology"},
+        {"slug": "key-fhir-resources",                  "text": "Key FHIR Resources"},
         {"slug": "fhir-community",                      "text": "FHIR Community"},
         {"slug": "fhir-research",                       "text": "FHIR for Research"},
-        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
-        {"slug": "fhir-from-10000-ft",                  "text": "FHIR from 10,000ft"},
-        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"},
-        {"slug": "redcap-intro",                        "text": "Introduction to REDCap"},
-        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
-        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
-        {"slug": "data-modeling-reading-igs",           "text": "Data Modeling: Reading IGs"},
         {"slug": "example-study",                       "text": "Examples of Real World Studies"},
+
+        {"slug": "data-modeling-api-resources",         "text": "Data Modeling: API Resources"},
+        {"slug": "data-modeling-reading-igs",           "text": "Data Modeling: Reading IGs"},
+        {"slug": "data-modeling-real-world-igs",        "text": "Data Modeling: Real World IGs"},
         {"slug": "data-modeling-writing-igs",           "text": "Data Modeling: Writing IGs"},
-        {"slug": "tabular-real-world",                  "text": "Tabular Data: Real World Cases"}
+
+        {"slug": "cdms",                                "text": "Common Data Models"},
+
+        {"slug": "tabular-retrieving-and-subsetting",   "text": "Tabular Data: Retrieving and Subsetting"},
+        {"slug": "tabular-from-fhir",                   "text": "Tabular Data: to and from FHIR"},
+        {"slug": "tabular-real-world",                  "text": "Tabular Data: Real World Cases"},
+
+        {"slug": "redcap-intro",                        "text": "Introduction to REDCap"},
+        {"slug": "redcap-example",                      "text": "REDCap Examples"},
+        {"slug": "redcap-best-practices",               "text": "REDCap Best Practices"},
+
+        {"slug": "smart-on-fhir-intro",                 "text": "Introduction to SMART on FHIR"},
+        {"slug": "smart-on-fhir-research",              "text": "SMART on FHIR for Research"},
+        {"slug": "smart-on-fhir-technical-quick-start", "text": "SMART on FHIR Technical Quickstart"}
     ]
   }
 ];
